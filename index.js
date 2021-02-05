c1937190b5003c11e6e91c54cf5bb51dc11a9be5
Ashley Burns <ashley@ashleys-mini.home>
2021-02-04 21:56:35 -0500

    task 4 done

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ reviews.push({
   rating: 5,
   feedback: "This place is fantastic!"
 })
-console.log(reviews)
+// console.log(reviews)
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Reyna's feedback is missing! Use what you know to do the following: (no function needed) 
@@ -113,8 +113,8 @@ Reyna's feedback is missing! Use what you know to do the following: (no function
   2. log the reviews array to the console to check your work
 */
 
-
-
+reviews[7].feedback ="this place is chill with really cool people, great for getting work done on weekdays"
+console.log(reviews[7])
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
